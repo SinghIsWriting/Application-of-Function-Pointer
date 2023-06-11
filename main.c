e10ad6158aac3a1310f84463cd6a2d8d0d762e7b
Singh <sabhisheksignh343104@gmail.com>
2023-06-11 19:22:35 +0530

    Comments added

--- main.c
+++ main.c
@@ -1,19 +1,14 @@
-/*
-
-Area of cricle using function pointers
-Make a function that calcualte the Euclidian distance between two points, then 
-Using function pointer make a function to calculate the area of circle of radius distance between two points given by user
-
-*/
 #include <stdio.h>
-#include <math.h>
+#include <math.h>      // for pow() and sqrt()
 
-#define PI 3.14
+#define PI 3.14   // Defining a macro PI
 
+// Defining a function that takes four integers of two points and returns Euclidian distance between both points
 float euclidian_distance(int x1, int y1, int x2, int y2){
     return sqrt(pow(x1-x2, 2) + pow(y1-y2, 2));
 }
 
+/* Defining a function that takes a function pointer with four integer parameters and four other integers, it returns area of the circle of radius distance between two points */
 float areaOfCircle(float (*fptr)(int, int, int, int), int x1, int y1, int x2, int y2)
 {
     return PI * pow(fptr(x1, y1, x2, y2), 2);
@@ -22,6 +17,7 @@ float areaOfCircle(float (*fptr)(int, int, int, int), int x1, int y1, int x2, in
 int main()
 {
     printf("\n\n********* Welcome to Euclidian Distance and Area of Circle ********\n\n");
+    // Declaring function pointer
     float (*fptr)(int, int , int , int);
     fptr = &euclidian_distance;
 
